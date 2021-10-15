05981b3886a3068339c5eaecd1aa1bdea9691acb
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-10-14 19:13:10 -0500

    se actualiza opcion de objetos productos

--- Front/src/assets/js/main.js
+++ Front/src/assets/js/main.js
@@ -9,15 +9,15 @@ window.onload = function(){
 
 
 productosDisp = [];
-categorias = ["Artesanal", "Escolares", "Accesorios", "Electronica", "Figuras", "Posters y Cuadros", "Peluches", "Hogar", "", "" ];
+categorias = ["Personalizable", "Escolares", "Accesorios", "Electronica", "Figuras", "Posters y Cuadros", "Peluches", "Hogar", "", "" ];
 
 
 productoEjemplo = {
   nombreDeProducto: "soy un ejemplo",
   precio = 200,
   decripcion = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas repudiandae vero sequi? Blanditiis voluptas odio voluptates, vitae dolorem esse vero, rerum magni, debitis corrupti soluta odit ipsum ipsam laborum.",
-  esArtesanal = true,
+  esPersonalizable = true,
   disponibles = 100,
-  categoria = 0,
-  imagenes = []
+  categoria = [0,1],
+  imagenes = ["url1", "url2"]
 };
