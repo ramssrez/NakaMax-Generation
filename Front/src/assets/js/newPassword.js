68a88d4056c20936f65b20a71bd90d0dd815841f
Carlos Alberto Chavarria Ortiz <carlosalberto.chavarriao@gmail.com>
2021-10-17 23:10:43 -0500

    Agregando funcionalidad con JS a userWindow trabajando en conjunto con verifyPassword y newPassword

---
+++ Front/src/assets/js/newPassword.js
@@ -0,0 +1,45 @@
+//Enlace HTML y JS
+const $newPassword = document.querySelector('#newPassword');
+const $confirmPassword = document.querySelector('#confirmPassword');
+const $savePassword =document.querySelector('#savePassword');
+const $show2 = document.querySelector('#show2');
+const $cancelNewPassword = document.querySelector('#cancelNewPassword');
+
+//funcion mostrar contraseñas
+$show2.addEventListener('change', ()=>{
+    if($newPassword.type == "password"){
+        $newPassword.type = "text";
+        $confirmPassword.type = "text";
+    }else{
+        $newPassword.type = "password";
+        $confirmPassword.type = "password";
+    }
+})
+
+//funcion verificar igualdad de contraseñas
+$savePassword.addEventListener('click', ()=>{
+    const newPassword = $newPassword.value;
+    if(newPassword !== ""){
+        if(newPassword !== $confirmPassword.value){
+            alert('La contraseñas no son iguales');
+            $newPassword.value = "";
+            $confirmPassword.value = "";
+        }else{
+            alert('nueva contraseña guardada');
+            document.location.href = ('/Front/09-userWindow.html');
+        } 
+    }
+    else{
+        alert("la contraseña no se ha escrito");
+    }
+})
+
+// cancelar nueva contraseña
+$cancelNewPassword.addEventListener('click', ()=>{
+    window.location.href = "/Front/09-userWindow.html";
+})
+
+// cancelar boton back navegador
+// window.location.hash="no-back-button";
+// window.location.hash="Again-No-back-button";//esta linea es necesaria para chrome
+// window.onhashchange=function(){window.location.hash="no-back-button";}
---
+++ Front/src/assets/js/verifyPassword.js
@@ -0,0 +1,56 @@
+// cancelar boton back navegador
+window.onload = function(){
+    window.location.hash="no-back-button";
+    window.location.hash="Again-No-back-button";//esta linea es necesaria para chrome
+    window.onhashchange=function(){window.location.hash="no-back-button";}
+}
+
+// Enlace HTML y JS
+const $inputPassword = document.getElementById('Vpassword');
+const $show = document.querySelector('#showPassword');
+const $buttonVerify = document.querySelector('#nextStepPassword');
+const $cancel = document.querySelector('#cancelVerify');
+
+//recuperando valores de sessionStorage para saber tipo de edición por la cual se esta verificando, cambio de contraseña o de correo
+let editField = sessionStorage.getItem('typeEditData');
+
+//funcion para mostrar contraseña
+$show.addEventListener('change', ()=>{
+    if($inputPassword.type == "password"){
+        $inputPassword.type = "text";
+    }else{
+        $inputPassword.type = "password";
+    }
+})
+
+const prueba = 'carlos';
+//funcion verificar contraseña
+$buttonVerify.addEventListener('click', ()=>{
+    const contraseña = $inputPassword.value;
+    if(contraseña !== ""){
+        if(contraseña === prueba){
+
+            //dirigiendo a pantallas según el campo que se quiera editar: correo o contraseña
+            if(editField === "NewPassword"){
+                document.location.href = "/Front/04-newPassword.html";
+                sessionStorage.removeItem('typeEditData');
+            }else{
+                document.location.href = "/Front/09-userWindow.html";
+                sessionStorage.removeItem('typeEditData');
+                sessionStorage.setItem("verify","verified");
+            }
+
+        }else{
+            $inputPassword.value = "";
+            alert('Contraseña incorrecta');
+        }
+    }else{
+        alert('Introduzca su contraseña')
+    }
+})
+
+//cancelar verificación
+$cancel.addEventListener('click', ()=>{
+    sessionStorage.removeItem('typeEditData');
+    window.location.href = "/Front/09-userWindow.html";
+})
