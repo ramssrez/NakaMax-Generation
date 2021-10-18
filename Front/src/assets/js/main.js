e831c3618512af5da21d47a23e04e5cb84f579a9
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-10-18 14:00:07 -0500

    se sube loader en index

--- Front/src/assets/js/main.js
+++ Front/src/assets/js/main.js
@@ -8,12 +8,12 @@ categorias = ["Artesanal", "Escolares", "Accesorios", "Electronica", "Figuras", 
 
 productoEjemplo = {
   nombreDeProducto: "soy un ejemplo",
-  precio = 200,
-  decripcion = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas repudiandae vero sequi? Blanditiis voluptas odio voluptates, vitae dolorem esse vero, rerum magni, debitis corrupti soluta odit ipsum ipsam laborum.",
-  esArtesanal = true,
-  disponibles = 100,
-  categoria = 0,
-  imagenes = []
+  precio: 200,
+  decripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas repudiandae vero sequi? Blanditiis voluptas odio voluptates, vitae dolorem esse vero, rerum magni, debitis corrupti soluta odit ipsum ipsam laborum.",
+  esArtesanal: true,
+  disponibles: 100,
+  categoria: [0,2,3],
+  imagenes: ['url','otro url']
 };
 
 window.onload = function(){
