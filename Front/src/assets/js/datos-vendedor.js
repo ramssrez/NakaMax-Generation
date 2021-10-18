ae9de318c7e00950359a05924b39eee70554f31c
paulaciddelprado <cid.6924@gmail.com>
2021-10-18 01:01:10 -0500

    Séptimo update dev-paula: acerca de nosotros y datos del vendedor, funcionalidades y responsive

---
+++ Front/src/assets/js/datos-vendedor.js
@@ -0,0 +1,9 @@
+const $name = document.querySelector('#name');
+const $apellido = document.querySelector('#apellido');
+const $rfc = document.querySelector('#rfc');
+const $nacimiento = document.querySelector('#nacimiento');
+const $telefono = document.querySelector('#telefono');
+const $email = document.querySelector('#email');
+const $password = document.querySelector('#password');
+const $cPassword = document.querySelector('#cPassword');
+
