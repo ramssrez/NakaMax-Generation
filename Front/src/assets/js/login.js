ddd6fa8ec697dd24b3232b8d22e452f7056f6992
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-18 12:28:41 -0500

    Integración de id con JS

---
+++ Front/src/assets/js/login.js
@@ -0,0 +1,7 @@
+const $email = document.querySelector("#inputEmailLogin");
+const $password = document.querySelector("#inputPasswordLogin");
+const $btnLogin = document.querySelector("#btnLogin");
+
+$btnLogin.addEventListener('click', () =>{
+    console.log("Esto esta funcionando");
+  });
