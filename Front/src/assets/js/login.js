f10131cefc36a5d8c153689a54ba520d43ee180e
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-18 19:10:28 -0500

    Integración de sweetAlert

--- Front/src/assets/js/login.js
+++ Front/src/assets/js/login.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2'
 const $email = document.querySelector("#inputEmailLogin");
 const $password = document.querySelector("#inputPasswordLogin");
 const $btnLogin = document.querySelector("#btnLogin");
@@ -9,5 +10,10 @@ $btnLogin.addEventListener('click', () =>{
       console.log("Esto no esta vacio");
     }
   });
-
-  
+ 
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Something went wrong!',
+    footer: '<a href="">Why do I have this issue?</a>'
+  })
