9871c7c44c0dbc79d93383e0ba9b93d082b2e814
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-28 13:15:09 -0500

    Creación de la tabla de contactanos

---
+++ Back/basedatos/crearTablas.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE nakamax;
+USE nakamax;
+
+/* Tabla de Contacta nos */
+CREATE TABLE cometarios_contac_us (
+  id_comentario INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(255) NULL,
+  correo VARCHAR(255) NOT NULL,
+  telefono VARCHAR(255) NULL,
+  comentario VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_comentario))
+ENGINE = InnoDB;
+
+/*  */
