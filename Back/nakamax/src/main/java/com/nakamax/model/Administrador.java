b74d494a681fbbcb1f3b44fcde860b1e16cf3c2f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-04 21:13:23 -0600

    Integracion de getters y setters

--- Back/nakamax/src/main/java/com/nakamax/model/Administrador.java
+++ Back/nakamax/src/main/java/com/nakamax/model/Administrador.java
@@ -8,15 +8,105 @@ public class Administrador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_admin")
     private Integer id;
+
     @Column(name = "nombre")
     private String nombre;
+
     @Column(name = "apellido_paterno")
     private String apellidoPaterno;
+
     @Column(name = "apellido_materno")
     private String apellidoMaterno;
+
     private String correo;
     private String password;
     private String imagen;
     private String github;
     private String linkedin;
+
+    public Administrador() {
+
+    }
+
+    public Administrador(Integer id) {
+        this.id = id;
+    }
+
+    public Administrador(Integer id, String nombre, String apellidoPaterno, String apellidoMaterno, String correo, String github, String linkedin) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.correo = correo;
+        this.github = github;
+        this.linkedin = linkedin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
 }
