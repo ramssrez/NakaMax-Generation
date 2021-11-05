58a2310cd3a68ef46d8e3f5064a7b2dc3721b41f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-04 21:20:15 -0600

    Creación del administrador servicio implementacion

---
+++ Back/nakamax/src/main/java/com/nakamax/service/AdministradorServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nakamax.service;
+
+import com.nakamax.model.Administrador;
+import com.nakamax.repository.AdministradorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class AdministradorServiceImpl implements AdministradorService{
+    private final AdministradorRepository administradorRepository;
+
+    public AdministradorServiceImpl(AdministradorRepository administradorRepository) {
+        this.administradorRepository = administradorRepository;
+    }
+
+    @Override
+    public ArrayList<Administrador> findAll() {
+        return null;
+    }
+}
