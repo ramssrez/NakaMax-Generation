237c9350569bc6b55915b9185ea71ba85e3bdd76
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-04 21:17:22 -0600

    Integración del servicio del administrador

---
+++ Back/nakamax/src/main/java/com/nakamax/service/AdministradorService.java
@@ -0,0 +1,9 @@
+package com.nakamax.service;
+
+import com.nakamax.model.Administrador;
+
+import java.util.ArrayList;
+
+public interface AdministradorService {
+    ArrayList<Administrador> findAll();
+}
