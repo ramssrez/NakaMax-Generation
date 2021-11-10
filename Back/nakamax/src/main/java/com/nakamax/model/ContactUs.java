0f5264e529d0574905e37859242496e3130e6b69
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-09 18:30:10 -0600

    Creción de modelo y sus metodos

--- Back/nakamax/src/main/java/com/nakamax/model/ContactUs.java
+++ Back/nakamax/src/main/java/com/nakamax/model/ContactUs.java
@@ -1,2 +1,61 @@
-package com.nakamax.model;public class ContactUs {
+package com.nakamax.model;
+
+public class ContactUs {
+    private Integer idComentario;
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String comentario;
+
+    public ContactUs() {
+
+    }
+
+    public ContactUs(Integer idComentario, String nombre, String correo, String telefono, String comentario) {
+        this.idComentario = idComentario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.comentario = comentario;
+    }
+
+    public Integer getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(Integer idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
