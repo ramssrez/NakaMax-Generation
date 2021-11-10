509f3f40249ad3cf8e2c856cfdad4e1df21efdb1
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-09 19:26:10 -0600

    Prueba de los metodo para agregar comentarios y eliminarlos

--- Back/nakamax/src/main/java/com/nakamax/controller/ControllerContactUs.java
+++ Back/nakamax/src/main/java/com/nakamax/controller/ControllerContactUs.java
@@ -1,12 +1,13 @@
 package com.nakamax.controller;
 
 import com.nakamax.model.ContactUs;
+import com.nakamax.model.Vendedor;
 import com.nakamax.service.ContactUsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 public class ControllerContactUs {
@@ -16,8 +17,32 @@ public class ControllerContactUs {
         this.contactUsService = contactUsService;
     }
 
+    //Obtiene los comentarios de los usuarios
     @GetMapping("/comentarios")
     public ArrayList<ContactUs> getAll(){
         return contactUsService.findAll();
+    }
+
+    //Obtienen los comentarios por id
+    @GetMapping("/comentario/{id}")
+    public Optional<ContactUs> getById(@PathVariable Integer id){
+        return contactUsService.findById(id);
+    }
+
+    @PostMapping("/comentario")
+    public void save(@RequestBody ContactUs contactUs){
+        contactUsService.saveContactUs(contactUs);
+    }
+
+    @PutMapping("/comentario")
+    public void update(@RequestBody ContactUs contactUs)
+    {
+        contactUsService.saveContactUs(contactUs);
+    }
+
+    @DeleteMapping("/comentario/{id}")
+    public void delete(@PathVariable Integer id)
+    {
+        contactUsService.delete(id);
     }
 }
