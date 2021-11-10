d05dd2733e7fe2f71259d509d82d979c1dc6efec
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-09 18:52:53 -0600

    Implementación de los metodos de la interface

--- Back/nakamax/src/main/java/com/nakamax/service/ContactUsService.java
+++ Back/nakamax/src/main/java/com/nakamax/service/ContactUsService.java
@@ -1,4 +1,16 @@
 package com.nakamax.service;
 
+import com.nakamax.model.ContactUs;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface ContactUsService {
+    ArrayList<ContactUs> findAll();
+
+    Optional<ContactUs> findById(Integer id);
+
+    void saveVendedor(ContactUs contactUs);
+
+    void delete(Integer id);
 }
